8d3908163aec2da3f35be00b7ca753a404fdb83e
SU <jingbosu2010@hotmail.com>
2015-05-21 14:03:30 +0200

    ajouter la fonctionnalite de la creation du fichier dans le repertoire temporaire

--- dispositif/Cubicle/savetemporary.cpp
+++ dispositif/Cubicle/savetemporary.cpp
@@ -1,5 +1,10 @@
 #include "savetemporary.h"
 #include "ui_savetemporary.h"
+#include <QDebug>
+#include <QFile>
+#include <QDir>
+#include <QIODevice>
+#include <QTextStream>
 
 SaveTemporary::SaveTemporary(QWidget *parent) :
     QDialog(parent),
@@ -11,4 +16,29 @@ SaveTemporary::SaveTemporary(QWidget *parent) :
 SaveTemporary::~SaveTemporary()
 {
     delete ui;
+}
+
+void SaveTemporary::on_buttonBox_accepted()
+{
+    QString texte_2 = ui -> textEdit_2 -> toPlainText();
+    QDir dir(texte_2);
+    if (!dir.exists()) {
+        dir.mkpath(".");
+        qDebug() << "Created directory successed!";
+    }
+    QString texte= ui -> textEdit -> toPlainText();
+    /*
+    QFile fileCreate(texte);
+    if (fileCreate.open(QIODevice::ReadWrite)){
+        QTextStream myStream(&fileCreate);
+        myStream<<texte;
+        qDebug() << "Created texte successed!";
+    }
+*/
+    QFile file;
+    file.setFileName(texte +".txt");//file_name is the QString, which I get as aparameter
+    file.open(QIODevice::ReadWrite | QIODevice::Text);
+    QTextStream stream(&file);
+    QTextStream(stdout) << texte; //To see the name of the file, which has just been typed
+    file.close();
 }
--- dispositif/Cubicle/savetemporary.h
+++ dispositif/Cubicle/savetemporary.h
@@ -15,6 +15,9 @@ public:
     explicit SaveTemporary(QWidget *parent = 0);
     ~SaveTemporary();
 
+private slots:
+    void on_buttonBox_accepted();
+
 private:
     Ui::SaveTemporary *ui;
 };
