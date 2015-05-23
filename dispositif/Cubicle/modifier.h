70c02219a189dcd656a5deb5dc6e279327a27b7a
wanwanyu <yuwanwan0111@hotmail.com>
2015-05-23 09:43:27 +0200

    ajouter deux fichiers qui sert a la modification d'une ligne specifiee

---
+++ dispositif/Cubicle/modifier.h
@@ -0,0 +1,11 @@
+#ifndef MODIFIER_H
+#define MODIFIER_H
+#include "QString"
+class modifier
+{
+public:
+    modifier();
+    void remplaceligne(QString remp,QString s,int a);
+};
+
+#endif // MODIFIER_H
