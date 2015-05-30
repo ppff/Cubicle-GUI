2e074ced04195956d054c5fe00dcb2f5fb8ac84c
Wafa BEN_HADJ_SGHAIER <wafa.ben-hadj-sghaier@ensimag.grenoble-inp.fr>
2015-05-30 11:24:16 +0200

    modification de création fichier et updatefichier en fonction du nouveau format fichier

--- dispositif/Cubicle/gestionfichier.cpp
+++ dispositif/Cubicle/gestionfichier.cpp
@@ -14,54 +14,48 @@ void GestionFichier::ouvrir(QString empl,Cube c){
     }
 
     QTextStream flux(&file);
-    QString readLine;
-    QString newLine;
-    for (int k=0;k<2;k++){
-        readLine= flux.readLine()+'\n';
-        newLine+=readLine;
-    }
-    int nplan=0;// numéro du plan
-    int nligne=0; //numéro de la ligne
-
+    QString x,y,z;
+    QString version=flux.readLine(); // on lit la ligne de la version
+    file.resize(0);
+    flux<<version<<endl;
    for(int cntP=0;cntP<9;cntP++){
+       y=QString::number(cntP);
        for(int cntL=0;cntL<9;cntL++){
-           readLine= flux.readLine()+'\n';
-           newLine+=getLinePlan(nplan,nligne,c)+'\n';
-           nligne++; //passer à la ligne suivante
+           z=QString::number(cntL);
+           for(int cntA=0;cntA<9;cntA++){
+               Led l=c.getList1()->value(cntP).getLed(cntL,cntA);
+               int e=l.getEtat() ;
+               if (e) {
+
+                   x=QString::number(cntA);
+                   qDebug()<<"led ("+x+y+z+") allumé ";
+                   flux<<x+y+z<<endl;
+           }
+
        }
-       for (int i=0;i<2;i++){//recopier la ligne vide et la ligne #2 par exemple
-        readLine= flux.readLine()+'\n';
-        newLine+=readLine;
-       }
-        nligne=0;
-        nplan++;// passer au plan suivant
    }
+
+
+
+}
    file.close();
-   modifierFichier(newLine,empl);
 }
 
 QString GestionFichier::getLinePlan(int nplan,int nligne,Cube c){
     QString str="";
-    QString etat;
+    QString x,y,z;
+    y=QString::number(nplan);
+    z=QString::number(nligne);
     for(int i=0;i<9;i++){
        Led l=c.getList1()->value(nplan).getLed(nligne,i);
        int e=l.getEtat() ;
-       etat=QString::number(e);
-       str+=etat;
+       if (e=1) {
+           x=QString::number(i);
+           str+=x+y+z+'\n';
+       }
     }
     return str;
 }
 
-void GestionFichier::modifierFichier(QString newLine, QString empl){
-    QFile file(empl);
-    if(!file.open(QIODevice::WriteOnly | QIODevice::Text)){
-        qDebug()<<"le fichier n'est pas ouvert";
-       return;
-    }
-    QTextStream out(&file);
-    out << newLine;
-    file.flush();
-    file.close();
-}
 
 
