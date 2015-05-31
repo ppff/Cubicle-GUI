ae73b667188fef739959cf26e3da7571d558a9b2
Wafa BEN_HADJ_SGHAIER <wafa.ben-hadj-sghaier@ensimag.grenoble-inp.fr>
2015-05-31 20:30:33 +0200

    corection du saveAs

--- dispositif/Cubicle/mainwindow.cpp
+++ dispositif/Cubicle/mainwindow.cpp
@@ -408,10 +408,7 @@ void MainWindow::xCopy2 (const QString &sourcePath, const QString &destPath, con
 
 
     }
-    namedir= destObjectPath;
-    qDebug()<< "le nouveau path est" + namedir;
-    this->setWindowTitle("Cubicle["+destObjectPath+"]") ;
-    tree();
+
 
 
 
@@ -429,6 +426,10 @@ void MainWindow::controlSaveAs(){
     if (!removeDir(namedir+"/Cubicle")){
         qDebug()<<namedir+"/Cubicle n'est pas supprimé";
     }
+    namedir= destPath+"/Cubicle";
+    qDebug()<< "le nouveau path est" + namedir;
+    this->setWindowTitle("Cubicle["+destPath+"/Cubicle"+"]") ;
+    tree();
 
 }
 
